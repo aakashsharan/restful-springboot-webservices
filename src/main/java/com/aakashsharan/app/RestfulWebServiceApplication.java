f4cfb91f69da83aeeb2e5d518bba665cd4baed01
Aakash Sharan <asharan@localhost.localdomain>
2019-01-01 18:57:10 -0600

    create main class and entity class

---
+++ src/main/java/com/aakashsharan/app/RestfulWebServiceApplication.java
@@ -0,0 +1,11 @@
+package com.aakashsharan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class RestfulWebServiceApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args){
+        SpringApplication.run(RestfulWebServiceApplication.class, args);
+    }
+}
